e1a742d21830a0214ef4095685e9fbd780b445dc
Imran Khalid <mimrankhalid448@gmail.com>
2025-12-25 18:55:15 +0500

    Updates

---
+++ .history/vite.config_20251206165406.js
@@ -0,0 +1,12 @@
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      'craniometrically-pseudoconfessional-esmeralda.ngrok-free.dev'
+    ],
+    host: true,
+    port: 5174
+  }
+});
